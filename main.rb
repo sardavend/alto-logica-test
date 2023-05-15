40b85cf7dc90fcda77157d90dd34579af8dfa41f
juanca sardin <juan.carlos.sardin@gmail.com>
2023-05-15 13:37:21 -0400

    Add entry point

---
+++ main.rb
@@ -0,0 +1,22 @@
+require_relative 'order_wrapper'
+require_relative 'order'
+
+puts "Enter a list of products with their respective cost. Type `summary` if you want to generate receipt."
+ORDER_PATTERN = /^(\d+) (.+) at (\d+\.\d{2})$/
+Orders = {}
+
+order_lines = []
+
+while line = gets
+  match = ORDER_PATTERN.match(line.chomp)
+  if match
+    order_line = OrderWrapper.parse_order_line(match)
+    order_lines.push(order_line)
+  end
+
+  break if line.chomp === 'summary'
+end
+
+order = Order.new(order_lines: order_lines)
+
+order.generate_receipt
